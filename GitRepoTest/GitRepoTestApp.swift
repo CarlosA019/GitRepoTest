9cd928dfcb31fa730a14382f54bc4e7bd6486678
CarlosA <cmalcala5487@gmail.com>
2023-09-30 11:22:05 -0500

    Initial Commit

---
+++ GitRepoTest/GitRepoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepoTestApp.swift
+//  GitRepoTest
+//
+//  Created by admin on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GitRepoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
